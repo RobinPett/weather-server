f72c75d31b0961e4bf2ade5a00b2f98f673f8ad2
rp222nc <rp222nc@student.lnu.se>
2025-06-07 18:46:01 +0200

    Setup base route

--- src/routes/api/router.js
+++ src/routes/api/router.js
@@ -9,3 +9,8 @@ import express from 'express'
 export const router = new express.Router()
 
 // router.use('/auth', authRouter)
+router.use('/', (req, res) => {
+  res.json({
+    message: 'Welcome to the API'
+  })
+})
